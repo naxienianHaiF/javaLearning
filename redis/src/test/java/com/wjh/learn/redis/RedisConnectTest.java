51a1a4d881cb0a4abb5c75f4709a93006d8023b4
WJH <1342633711@qq.com>
2018-04-22 20:00:18 +0800

    jedis连接池demo

--- redis/src/test/java/com/wjh/learn/redis/RedisConnectTest.java
+++ redis/src/test/java/com/wjh/learn/redis/RedisConnectTest.java
@@ -2,6 +2,8 @@ package com.wjh.learn.redis;
 
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author JHW
@@ -31,5 +33,36 @@ public class RedisConnectTest {
         System.out.println(jedis.get("name"));
 
         jedis.close();
+    }
+
+    /*
+    连接池测试
+     */
+    @Test
+    public void jedisPoolTest(){
+        //连接池的配置对象
+        JedisPoolConfig config = new JedisPoolConfig();
+        //设置最大连接数
+        config.setMaxTotal(10);
+        //设置最大空闲连接数
+        config.setMaxIdle(5);
+
+        //获取连接池
+        JedisPool pool = new JedisPool(config,"192.168.3.173",6379);
+
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            System.out.println(jedis.ping());
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (jedis != null){
+                jedis.close();
+            }
+            if (pool != null){
+                pool.close();
+            }
+        }
     }
 }
