e8e97793e0b6dc44b6e619dd435cac50933db0b2
WJH <1342633711@qq.com>
2018-06-03 21:17:06 +0800

    队列模式添加相关注释

--- jsm-spring/src/main/java/com/wjh/jms/consumer/AppConsumer.java
+++ jsm-spring/src/main/java/com/wjh/jms/consumer/AppConsumer.java
@@ -4,7 +4,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Created by WJH on 2017年12月17日 21:15
+ * @author JHW
+ * @date 2017年12月17日 21:15
+ * 需要启动 E:\software\activemq\apache-activemq-5.15.2\bin路径下的ActiveMQ
+ * 队列模式（本代码是队列模式）
+ * <p>&nbsp;&nbsp; 队列模式的消费者平分所有的消息，主题模式是每个消费者获得所有的消息</p>
  */
 public class AppConsumer {
     public static void main(String[] args) {
